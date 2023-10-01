54b8b88e85c1350f2198c0cf6d713f253e30226e
Levin-ops <artmwanganyi09@outlook.com>
2023-10-01 19:32:25 +0300

    To do List Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector(`form`)
+  form.addEventListener(`submit`, function(event){
+    event.preventDefault()
+    toDoHandler(event.target.new_task_description.value);
+    form.reset()
+  });
+  
 });
+
+function toDoHandler(todo){
+  let p = document.createElement(`p`)
+  let btn =document.createElement(`button`)
+  btn.textContent=`X`
+  btn.addEventListener('click', toDoDeleter)
+  p.textContent=`${todo}  `
+  p.appendChild(btn)
+ 
+  document.querySelector(`#tasks`).appendChild(p)
+}
+
+function toDoDeleter(event){
+  event.target.parentNode.remove()
+}
